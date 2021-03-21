5faa330cc525c2cbf1689a5d21ff5b6ab7c1de97
jmsaylor <johnmsaylor@gmail.com>
2021-03-21 11:39:40 -0400

    minor changes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const { example } = require("./example");
 const app = express();
+const cors = require("cors");
 
 const port = 9000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/example", async (req, res) => {
+  console.log(await req.body);
   const { action, payload } = await req.body;
   const value = await example(action, payload);
   console.log("RESPONSE: " + value);
-  res.send(value);
+  const body = {
+    value: value,
+  };
+  res.json(body);
 });
 
 app.listen(port, () => {
